eda08f9c1accc994f6558a5dfbca0603d1b49096
Walter Dal Mut <walter.dalmut@gmail.com>
2013-10-21 09:56:52 +0200

    Removed -t option

--- bin/start-cluster.sh
+++ bin/start-cluster.sh
@@ -14,7 +14,7 @@ if sudo docker ps | grep "wdalmut/mongodb" >/dev/null; then
     exit 1
 fi
 
-SHARD0_ID=$(sudo docker run -d -t wdalmut/mongodb mongod -f /etc/mongodb.conf  --logpath /dev/stdout --bind_ip 0.0.0.0 --port 10010)
+SHARD0_ID=$(sudo docker run -d wdalmut/mongodb mongod -f /etc/mongodb.conf  --logpath /dev/stdout --bind_ip 0.0.0.0 --port 10010)
 SHARD0_IP=$(sudo docker inspect ${SHARD0_ID} | grep "IPAddress" | cut -d':' -f2 | cut -d'"' -f2)
 echo "Your shard container ${SHARD0_ID} listen on ip: ${SHARD0_IP} (waiting that becomes ready)"
 
@@ -25,7 +25,7 @@ done
 
 echo "The shard is available now..."
 
-CONFIG0_ID=$(sudo docker run -d -t wdalmut/mongodb mongod -f /etc/mongodb.conf --configsvr --logpath /dev/stdout --bind_ip 0.0.0.0 --port 10000)
+CONFIG0_ID=$(sudo docker run -d wdalmut/mongodb mongod -f /etc/mongodb.conf --configsvr --logpath /dev/stdout --bind_ip 0.0.0.0 --port 10000)
 CONFIG0_IP=$(sudo docker inspect ${CONFIG0_ID} | grep "IPAddress" | cut -d':' -f2 | cut -d'"' -f2)
 echo "Your config container ${CONFIG0_ID} listen on ip: ${CONFIG0_IP} (waiting that becomes ready)"
 
@@ -36,7 +36,7 @@ done
 
 echo "The config is available now..."
 
-MONGOS0_ID=$(sudo docker run -d -t wdalmut/mongodb mongos --configdb ${CONFIG0_IP}:10000 --logpath /dev/stdout --bind_ip 0.0.0.0 --port 9999)
+MONGOS0_ID=$(sudo docker run -d wdalmut/mongodb mongos --configdb ${CONFIG0_IP}:10000 --logpath /dev/stdout --bind_ip 0.0.0.0 --port 9999)
 MONGOS0_IP=$(sudo docker inspect ${MONGOS0_ID} | grep "IPAddress" | cut -d':' -f2 | cut -d'"' -f2)
 echo "Contacting shard and mongod containers"
 
